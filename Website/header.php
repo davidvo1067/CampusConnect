78f129e5199d6bf34ba46647ad879af24c17949d
Jojo <Jocelyn.KY.Lee@gmail.com>
2019-04-22 16:48:06 -0700

    Adding website to repository

---
+++ Website/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans|Pacifico|Material+Icons" rel="stylesheet">
+		<link href="styles.css" rel="stylesheet">
+		<link rel="import" href="profile.html">
+		<title>Campus Connect</title>
+	</head>
+	<body>
+		<div id="header">
+			Campus Connect!
+			<div id="search">
+				<form action="search.php">
+					<input type="text" placeholder="Search..." name="keyword">
+					<button type="submit"><i class="material-icons">search</i></button>
+				</form>
+			</div>
+		</div>
+		
+		<div id="sidebar">
+			<ul>
+				<li><a href="home.php">Home</a></li>
+				<li><a href="events.php">Events</a></li>
+				<li><a href="groups.php">Groups</a></li>
+				<li><a href="calendar.php">Calendar</a></li>
+				<li><a href="classes.php">Classes</a></li>
+				<li><a href="profile.php">Profile</a></li>
+				<li><a href="messages.php">Messages</a></li>
+			</ul>
+		</div>
+		<div id="content">
---
+++ Website/profile.php
@@ -0,0 +1,22 @@
+<?php include 'header.php'; ?>
+
+			<div id="profile">
+				<div id="about">
+					<div id="pic">
+						<img>
+						<div id="quick-icons">
+							<img class="profile-pic" src="harryProfile.jpg" alt="Profile picture" height="200px" width="200px">
+						</div>
+					</div>
+					<div id="summary">
+						<h1>Anon Ymous</h1>
+						<p>Batchelors of Wizardry, 1997</p>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras non ex quis augue imperdiet tempor. Mauris fermentum malesuada elit, quis aliquet enim dictum quis. Ut sit amet ex sit amet nisl aliquam fringilla. Ut eget sagittis massa, vitae consequat nibh. In hac habitasse platea dictumst. Donec in turpis eget enim imperdiet maximus. Nullam fermentum quis leo in aliquam. Mauris ut sem viverra, volutpat nibh sit amet, aliquam enim. Aenean turpis urna, mattis id ligula eu, varius laoreet augue. Aliquam quis condimentum justo, sit amet efficitur nunc. Cras id ex mauris. Nullam tristique nulla sit amet nisi aliquet pulvinar. Ut tincidunt lacus eu urna mattis, id feugiat tellus euismod. Proin iaculis nisi non justo convallis, ut varius enim pellentesque.</p>
+					</div>
+				</div>
+				<div id="feed">
+					<p>Pretending there's a feed</p>
+					<p>Yup totally a feed</p>
+				</div>
+			</div>
+<?php include 'footer.php'; ?>
